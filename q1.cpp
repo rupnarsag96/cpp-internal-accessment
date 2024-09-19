cb89d62e9b4c22669b619b6ce497a9326ba4fea9
rupnarsag96 <rupnarsagar709@gmail.com>
2024-09-20 00:04:24 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,59 +1,436 @@
 /*Q.No 1. Bookshop Management System
 File handling has been effectively used for each feature of this project
 Operations
-1. Add Book Records:
-2. Show Book Records:
-3. Check Availability:
-4. Modify Book Records:
-5. Delete Book Records:*/
-#include<iostream>
-#include<fstream>
- using namespace std;
- int main()
- {
- 	string name; 
-	string aut;
- 	int price;
- 	cout<<"Enter book name, authour, price of book";
- 	cin>>name>>aut>>price;
- 	ofstream out("bookstore.txt");//add
- 	out<<"\nname"<<name<<"\nauthor"<<aut<<"\nprice"<<price;
- 	out.close();
- 	ifstream in("bookstore.txt");//show
- 	in>>name>>aut>>price;
- 	in.close();
- 	string line;
- 	ofstream out1;
- 	out1.open("bookstore.txt", ios::app);//modify records
- 	cout<<"TO GET OUT SIDE FROM FILE ENTER 'QUIT'";
- 	while(getline(cin,line))
- 	{
- 	if(line=='quit'||line=='QUIT')
- 	{
-	 break;
+1. Add Book Records:
+2. Show Book Records:
+3. Check Availability:
+4. Modify Book Records:
+5. Delete Book Records: */
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Book {
+    int id;
+    string t;
+    string a;
+    int q;
+
+    void display() {
+        cout<<"ID:"<<id<<",Title:"<<t<<",Author:"<<a<<",Quantity:"<<q<< endl;
     }
-    else
-    out<<"\n"<<line;
-	}
- 	out1.close();
- 	string line3;
- 	ifstream in3("bookstore.txt");// to read
- 	while(in.is_open())
- 	{
- 	if(getline(in3,line3))	
- 	{
- 	in3<<"\n"<<line3;	
-	 }
-	 else
-	 {
-	 cout<<"file is no present in file";	
-	 }
-	}
-	ofstream out4("bookstore.txt");//delete 
- 	out4<<"\nname"<<name<<"\nauthor"<<aut<<"\nprice"<<price;
- 	out4.close();
- 	ifstream in4("bookstore.txt");//show
- 	in4>>name>>aut>>price;
- 	in4.close();
- 	return 0;
- 	}
+};
+
+void addBook();
+void showBooks();
+void checkAvailability();
+void modifyBook();
+void deleteBook();
+
+int main() {
+    int choice;
+    do {
+        cout << "\nBookshop Management System\n";
+        cout << "1. Add Book Records\n";
+        cout << "2. Show Book Records\n";
+        cout << "3. Check Availability\n";
+        cout << "4. Modify Book Records\n";
+        cout << "5. Delete Book Records\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: addBook(); break;
+            case 2: showBooks(); break;
+            case 3: checkAvailability(); break;
+            case 4: modifyBook(); break;
+            case 5: deleteBook(); break;
+            case 0: cout << "Exiting...\n"; break;
+            default: cout << "Invalid choice! Please try again.\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}
+
+void addBook() {
+    ofstream outFile("bookstore.txt", ios::app);
+    Book book;
+    cout << "Enter Book ID: ";
+    cin >> book.id;
+    cin.ignore();
+    cout << "Enter Book Title: ";
+    getline(cin, book.t);
+    cout << "Enter Author Name: ";
+    getline(cin, book.a);
+    cout << "Enter Quantity: ";
+    cin >> book.q;
+
+    outFile << book.id << endl;
+    outFile << book.t << endl;
+    outFile << book.a << endl;
+    outFile << book.q << endl;
+    outFile.close();
+    cout << "Book added successfully!\n";
+}
+
+void showBooks() {
+    ifstream inFile("bookstore.txt");
+    Book book;
+    cout << "\nBook Records:\n";
+    while (inFile >> book.id) {
+        inFile.ignore();
+        getline(inFile, book.t);
+        getline(inFile, book.a);
+        inFile >> book.q;
+        inFile.ignore();
+        book.display();
+    }
+    inFile.close();
+}
+
+void checkAvailability() {
+    ifstream inFile("bookstore.txt");
+    Book book;
+    int id;
+    cout << "Enter Book ID to check availability: ";
+    cin >> id;
+
+    bool found = false;
+    while (inFile >> book.id) {
+        inFile.ignore();
+        getline(inFile, book.t);
+        getline(inFile, book.a);
+        inFile >> book.q;
+        inFile.ignore();
+        
+        if (book.id == id) {
+            found = true;
+            cout << "Book is available. Quantity: " << book.q<< endl;
+            break;
+        }
+    }
+    if (!found) {
+        cout << "Book not found!\n";
+    }
+    inFile.close();
+}
+
+void modifyBook() {
+    fstream file("bookstore.txt", ios::in | ios::out);
+    Book book;
+    int id, pos = -1;
+
+    cout << "Enter Book ID to modify: ";
+    cin >> id;
+
+    while (file >> book.id) {
+        file.ignore();
+        getline(file, book.t);
+        getline(file, book.a);
+        file >> book.q;
+        file.ignore();
+
+        if (book.id == id) {
+            pos = file.tellg();
+            cout << "Enter new Title: ";
+            cin.ignore();
+            getline(cin, book.t;
+            cout << "Enter new Author: ";
+            getline(cin, book.a);
+            cout << "Enter new Quantity: ";
+            cin >> book.q;
+
+            file.seekp(pos - sizeof(Book) * 3);
+            file << book.id << endl;
+            file << book.t << endl;
+            file << book.a << endl;
+            file << book.q << endl;
+            cout << "Book modified successfully!\n";
+            break;
+        }
+    }
+    if (pos == -1) {
+        cout << "Book not found!\n";
+    }
+    file.close();
+}
+
+void deleteBook() {
+    ifstream inFile("bookstore.txt");
+    ofstream outFile("book.txt");
+    Book book;
+    int id;
+
+    cout << "Enter Book ID to delete: ";
+    cin >> id;
+
+    bool found = false;
+    while (inFile >> book.id) {
+        inFile.ignore();
+        getline(inFile, book.t);
+        getline(inFile, book.a);
+        inFile >> book.q;
+        inFile.ignore();
+
+        if (book.id == id) {
+            found = true;
+            cout << "Book deleted successfully!\n";
+            continue;
+        }
+        outFile << book.id << endl;
+        outFile << book.t << endl;
+        outFile << book.a << endl;
+        outFile << book.q<< endl;
+    }
+    if (!found) {
+        cout << "Book not found!\n";
+    }
+
+    inFile.close();
+    outFile.close();
+    remove("bookstore.txt");
+    rename("book.txt", "bookstore.txt");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
