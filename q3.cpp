4cc87b8f313edde6cd43dd61a640c24983270316
rupnarsag96 <rupnarsagar709@gmail.com>
2024-09-19 23:47:17 +0530

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -2,59 +2,49 @@
 of Employee and Salary. For 10 Employees Print list of all employees.
 Print list of employee who got heights salary.*/
  
- #include<iostream>
- using namespace std;
- class emp
- {
- 	private:
- 		int id[100],sal[100];
- 		string n[100];
- 	public:
- 		void get_info(int x[],string y[] ,int z[])
- 		{
- 		 for(int i=0;i<10;i++)
-		  {
-			id[i]=x[i];
- 			n[i]=y[i];
- 			sal[i]=z[i];
- 		}
-		 }
-		 void print_info()
-		 {
-		 	for(int i=0;i<10;i++)
-		 	{
-			 
-		 	cout<<"\nid"<<id<<"\nname"<<"\nsalary"<<n<<sal;
-		    }
-		 compare(sal);
-		 }
-		 
-	int compare(int sal[])
- 	    {
- 	 	int max=0;
- 		for(int i=0;i<10;i++)
- 		{
- 		 if(max<sal[i])
- 		 {
- 		 	max=sal[i];
-		  }
- 		}
- 		cout<<"max sal of employee"<<max;
-	 }
-};
-int main()
+#include<iostream>
+using namespace std;
+class employee
 {
-	int id1[100],sal1[100],i;
-	char n1[100];
-	cout<<"\nEnter id name and salary of employee ";
-	for(i=0;i<10;i++)
+	private:
+		
+		int eno,sal;
+		char name[20];
+		public:
+	void get()
 	{
-	cin>>id1[i]>>n1>>sal1[i];
-	emp s[10];
-	s[i].get_info(id1,sal1);
-	s[i].print_info();
-   }
-   
-	return 0;
+	cout<<"\n enter eno name and salary\n";
+	cin>>eno>>name>>sal;
+		
+	}	
+	void show()
+	{
+	cout<<"\neno="<<eno<<"\name="<<name<<"\nsalary="<<sal;
+	
+	}
+	int salary()
+	{
+		return(sal);
+    }
+};
+main()
+{
+	employee emp_obj[20];
+	int max=0,i;
+	for( i=0;i<3;i++)
+	{
+	emp_obj[i].get();
+	emp_obj[i].show();
+		}
+	
+	for( i=0;i<3;i++)
+	{
+		if(emp_obj[i].salary()>max)
+		max=emp_obj[i].salary();
+	}
+	cout<<"\n highest salary of employee="<<max;
 }
- 
+	
+
+	
+	
